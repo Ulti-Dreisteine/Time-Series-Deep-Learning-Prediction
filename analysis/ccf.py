a6d7a8a4d080f1cbb0692f9580f11d90a48bf59b
dreisteine262 <luolei@rockontrol.com>
2019-07-10 17:32:36 +0800

    1. 加入ccf时滞和相关性分析； 2. 根据ccf分析结果绘制变量相互作用图；

--- analysis/ccf.py
+++ analysis/ccf.py
@@ -8,6 +8,7 @@ Created on Mon Oct  8 14:32:52 2018
 """
 import pandas as pd
 import numpy as np
+import json
 import matplotlib.pyplot as plt
 import sys
 
@@ -17,109 +18,123 @@ from mods.config_loader import config
 from mods.data_filtering import savitzky_golay_filtering
 
 
-def cross_correlation(x_series, y_series, d):
+def cross_correlation(series_a, series_b, d, eps = 1e-6):
 	"""
-	在延迟为d上的互相关分析, x_series, 移动y_series: d > 0向右移，d < 0向左移
-	:param time_series_0: np.ndarray, 目标变量
-	:param time_series_1: np.ndarray, 外生变量
-	:param d: 延迟阶数
+	在延迟为d上的互相关分析, series_a, 移动series_b: d > 0向右移，d < 0向左移
+	:param series_a: np.ndarray, 目标变量, shape = (-1,)
+	:param series_b: np.ndarray, 外生变量, shape = (-1,)
+	:param d: time delay, 延迟阶数
 	:return:
 	"""
-	len_y = len(y_series)
-	y_series = np.hstack((y_series[-(d % len_y):], y_series[: -(d % len_y)]))
+	try:
+		assert type(series_a) == type(series_b) == np.ndarray
+		assert len(series_a.shape) == len(series_b.shape) == 1
+		assert len(series_a) == len(series_b)
+	except Exception:
+		raise ValueError('输入数据格式不正确')
 	
-	mean_x, mean_y = np.mean(x_series), np.mean(y_series)
-	numerator = np.sum((x_series - mean_x) * (y_series - mean_y))
-	denominator = np.sqrt(np.sum(np.power((x_series - mean_x), 2))) * np.sqrt(
-		np.sum(np.power((y_series - mean_y), 2)))
+	series_x = series_a.copy()
+	series_y = series_b.copy()
 	
-	return numerator / denominator
+	len_y = len(series_y)
+	series_y = np.hstack((series_y[-(d % len_y):], series_y[: -(d % len_y)]))
+	
+	mean_x, mean_y = np.mean(series_x), np.mean(series_y)
+	numerator = np.sum((series_x - mean_x) * (series_y - mean_y))
+	denominator = np.sqrt(np.sum(np.power((series_x - mean_x), 2))) * np.sqrt(
+		np.sum(np.power((series_y - mean_y), 2)))
+	
+	return numerator / (denominator + eps)
 
 
-def peak_loc_and_value(x_series, y_series, detect_len):
+def mean_ccf(series_a, series_b, d, seg_len, start_locs):
+	""""""
+	series_list = []
+	try:
+		assert start_locs[0] + d >= 0
+	except Exception:
+		raise ValueError('start locs起始位置不能被分析')
+	
+	try:
+		assert start_locs[-1] + d <= len(series_a) - 1
+	except Exception:
+		raise ValueError('start locs末尾位置不能被分析')
+	
+	for start_loc in start_locs:
+		series_list.append(
+			[
+				series_a.copy()[start_loc: start_loc + seg_len],
+				series_b.copy()[(start_loc + d): (start_loc + seg_len + d)]])
+	
+	series_list = pd.DataFrame(series_list, columns = ['series_a', 'series_b'])
+	series_list['ccf_value'] = series_list.apply(lambda x: cross_correlation(x[0], x[1], 0), axis = 1)
+	ccf = np.mean(list(series_list['ccf_value']))
+	
+	return ccf
+
+
+def time_delay_ccf_func(series_a, series_b, d_list, seg_len, start_locs):
 	"""
-	检测峰值
-	:param x_series:
-	:param y_series:
-	:param detect_len: 检测半长
-	:return:
+	含时滞计算的ccf
+	:param series_a: array
+	:param series_b: array
+	:param d_list: list or array
+	:param seg_len: int
+	:param start_locs: list or array
+	:return: time_delay_ccf, dict
 	"""
-	ccf_results = []
-	series_len = len(x_series)
-	for d in range(-detect_len, detect_len + 1):
-		ccf_results.append(cross_correlation(x_series, y_series, d))
+	time_delay_ccf = {}
+	for d in d_list:
+		time_delay_ccf[int(d)] = mean_ccf(series_a, series_b, d, seg_len, start_locs)
 	
-	mean, sigma = np.mean(ccf_results), np.power(np.var(ccf_results), 0.5)
-	pos_corr_values = [p for p in ccf_results if (p > mean + 3.0 * sigma)]
-	neg_corr_values = [p for p in ccf_results if (p < mean - 3.0 * sigma)]
-	
-	start_loc = detect_len - series_len // 2
-	end_loc = detect_len + series_len // 2 + 1
-	ccf_seg = ccf_results[start_loc: end_loc]
-	if (len(pos_corr_values) == 0) & (len(neg_corr_values) == 0):
-		peak_value = 0
-		peak_loc = 0
-	else:
-		if len(pos_corr_values) >= len(neg_corr_values):
-			peak_value = max(ccf_seg) - mean
-			peak_loc = ccf_seg.index(max(ccf_seg)) + start_loc
-		else:
-			peak_value = mean - min(ccf_seg)
-			peak_loc = ccf_seg.index(min(ccf_seg)) + start_loc
-	
-	time_lag = peak_loc - detect_len
-	
-	return ccf_seg, start_loc, end_loc, detect_len, time_lag, peak_value
+	return time_delay_ccf
 
 
 if __name__ == '__main__':
+	# 参数设定
+	columns = config.conf['model_params']['continuous_columns']
+	target_columns = config.conf['model_params']['target_columns']
+	
+	# 载入数据
 	data = pd.read_csv('../tmp/total_implemented_normalized_data.csv')
 	data = savitzky_golay_filtering(data)
 	
-	columns = config.conf['model_params']['continuous_columns']
-	diagnosis_locs = np.arange(0, 20000, 3000)
-	series_len = 200
-	detect_len = 5000
+	# CCF时滞计算
+	d_list = np.arange(-100, 102, 2)
+	seg_len = 1000  # 单次ccf计算选取的序列长度
+	start_locs = np.arange(1000, 22000, 100)
 	
-	ccf_and_time_lags = {}
-	plt.figure(figsize = [14, 14])
+	plt.figure(figsize = [10, 14])
+	total_time_delay_ccf = {}
 	for col_x in columns:
-		ccf_and_time_lags[col_x] = {}
-		for col_y in columns:
+		total_time_delay_ccf[col_x] = {}
+		for col_y in target_columns:
 			print('processing col_x: {}, col_y: {}'.format(col_x, col_y))
-			ccf_segs, time_lags, peak_values = [], [], []
-			for loc in diagnosis_locs:
-				x_series = np.array(data[col_x]).flatten()[loc: loc + series_len]
-				y_series = np.array(data[col_y]).flatten()[loc: loc + series_len]
-				ccf_seg, start_loc, end_loc, detect_len, time_lag, peak_value = peak_loc_and_value(x_series, y_series, detect_len)
-				ccf_segs.append(ccf_seg)
-				if (time_lag != -detect_len) & (peak_value != 0):
-					time_lags.append(time_lag)
-					peak_values.append(peak_value)
-			mean_ccf_seg = np.mean(np.array(ccf_segs), axis = 0)
-			mean_time_lag = np.mean(np.array(time_lags), axis = 0)
-			mean_peak_value = np.mean(np.array(peak_values), axis = 0)
+			series_a, series_b = np.array(data[col_x]), np.array(data[col_y])
+			time_delay_ccf = time_delay_ccf_func(series_a, series_b, d_list, seg_len, start_locs)
+			total_time_delay_ccf[col_x][col_y] = time_delay_ccf
 			
-			ccf_and_time_lags[col_x][col_y] = {'ccf_value': mean_peak_value, 'time_lag': mean_time_lag}
-			
-			plt.subplot(len(columns), len(columns), len(columns) * columns.index(col_x) + columns.index(col_y) + 1)
-			plt.plot(range(start_loc - detect_len, end_loc - detect_len), np.abs(mean_ccf_seg))
-			plt.fill_between(range(start_loc - detect_len, end_loc - detect_len), np.abs(mean_ccf_seg))
-			plt.plot([start_loc - detect_len, end_loc - detect_len], [0, 0], 'k--', linewidth = 0.3)
+			plt.subplot(
+				len(columns), len(target_columns), len(target_columns) * columns.index(col_x) + target_columns.index(col_y) + 1)
+			plt.plot(time_delay_ccf.keys(), np.abs(list(time_delay_ccf.values())))
+			plt.fill_between(time_delay_ccf.keys(), np.abs(list(time_delay_ccf.values())))
+			plt.plot([d_list[0], d_list[-1]], [0, 0], 'k--', linewidth = 0.3)
 			plt.plot([0, 0], [-1.0, 1.0], 'k-', linewidth = 0.3)
-			plt.xlim([-series_len // 2, series_len // 2])
+			plt.xlim([d_list[0], d_list[-1]])
 			plt.ylim([-1, 1])
 			plt.xticks(fontsize = 6)
 			plt.yticks(fontsize = 6)
 			if col_x == columns[0]:
 				plt.title(col_y, fontsize = 8)
 			
-			if col_y == columns[0]:
+			if col_y == target_columns[0]:
 				plt.ylabel(col_x, fontsize = 8)
 			
 			plt.tight_layout()
 			plt.show()
 			plt.pause(1.0)
 	
-	plt.tight_layout()
-	plt.savefig('../tmp/ccf_analysis_on_continuous.png')
+	plt.savefig('../graphs/ccf_analysis_on_continuous.png')
+	
+	with open('../tmp/total_time_delay_ccf_results.json', 'w') as f:
+		json.dump(total_time_delay_ccf, f)
